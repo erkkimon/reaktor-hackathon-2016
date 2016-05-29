2e30ccb40b9d0f987747d6119b73b8e0ba069eb3
Niki Ahlskog <niki.ahlskog@gmail.com>
2016-05-29 12:39:41 +0300

    kielipeli

--- kotonaattori/www/js/controllers.js
+++ kotonaattori/www/js/controllers.js
@@ -1,30 +1,104 @@
 angular.module('app.controllers', [])
-  
+
 .controller('kotonaattoriCtrl', function($scope) {
 
 })
-   
+
 .controller('faktaCtrl', function($scope) {
 
 })
-      
+
 .controller('kYtNnTCtrl', function($scope) {
 
 })
-   
+
 .controller('suomalaisuusCtrl', function($scope) {
 
 })
-   
+
 .controller('kieliCtrl', function($scope) {
 
+
+  var randomSanat = [
+  								{sana: 'Auto'},
+  								{sana: 'Vene'},
+  								{sana: 'Ankkuri'},
+  								{sana: 'Kissa'},
+  								{sana: 'Koira'},
+  								{sana: 'Leivinpaperi'}
+  							];
+
+  var vastaukset = [
+  								{sana: 'auto', kuva: 'http://alturl.com/6hfxb', vastaus: 'joo'},
+  								{sana: 'keksi', kuva: 'http://alturl.com/69r7g', vastaus: 'joo'}
+  							];
+
+  var items = [];
+
+  getRandom();
+
+  function getRandom(){
+  	var rand1 = getOneRandom();
+  	var rand2 = getOneRandom();
+  	var rand3 = getAnswer();
+  	if (rand1 === rand2){
+  		rand2 = getOneRandom();
+  	}
+  	items.push(rand1, rand2, rand3);
+    $scope.vaihtoehdot = shuffle(items);
+
+  	for (var i = 0; i < $scope.vaihtoehdot.length; i++) {
+  		if($scope.vaihtoehdot[i].kuva){
+  			$scope.kuva = $scope.vaihtoehdot[i].kuva;
+  		}
+  	}
+  };
+
+  function getOneRandom () {
+  	return randomSanat[Math.floor(Math.random() * randomSanat.length)];
+  }
+
+  function getAnswer () {
+  	return vastaukset[Math.floor(Math.random() * vastaukset.length)];
+  }
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  $scope.checkAnswer = function(answer){
+  	if (answer === "joo"){
+  		$scope.tulos = "oikein";
+  	}else {
+  		$scope.tulos = "vaarin"
+  	}
+  }
+
+  $scope.again = function() {
+  	window.location.reload()
+  }
+
 })
-   
+
 .controller('turvapaikkahakemusCtrl', function($scope) {
 
 })
-   
+
 .controller('tehtVCtrl', function($scope) {
 
 })
- 
